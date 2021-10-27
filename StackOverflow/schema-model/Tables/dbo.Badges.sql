b4d33a6de530c60758dc3d5a39647cd2da1d941f
WIN2016\redgate <redgate@redgate.com>
2021-10-27 22:36:45 +0100

    badges

--- StackOverflow/schema-model/Tables/dbo.Badges.sql
+++ StackOverflow/schema-model/Tables/dbo.Badges.sql
@@ -3,8 +3,7 @@ CREATE TABLE [dbo].[Badges]
 [Id] [int] NOT NULL IDENTITY(1, 1),
 [Name] [nvarchar] (40) NOT NULL,
 [UserId] [int] NOT NULL,
-[Date] [datetime] NOT NULL,
-[We_dont_need_no_stinking_badges] [nvarchar] (max) NULL
+[Date] [datetime] NOT NULL
 )
 GO
 ALTER TABLE [dbo].[Badges] ADD CONSTRAINT [PK_Badges__Id] PRIMARY KEY CLUSTERED ([Id])
