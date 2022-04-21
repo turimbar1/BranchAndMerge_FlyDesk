3ae650bdc8c99891996ef618e7a96af9dd576f8f
Andrew Pierce <andrew.craig.pierce@gmail.com>
2022-04-21 13:48:22 -0700

    Reset

---
+++ StackOverflow/migrations/B001_20220421130480__baseline.sql
@@ -0,0 +1,77 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[Comments]'
+GO
+CREATE TABLE [dbo].[Comments]
+(
+[Id] [int] NOT NULL IDENTITY(1, 1),
+[CreationDate] [datetime] NOT NULL,
+[PostId] [int] NOT NULL,
+[Score] [int] NULL,
+[Text] [nvarchar] (700) NOT NULL,
+[UserId] [int] NULL,
+[Length] [int] NULL
+)
+GO
+PRINT N'Creating primary key [PK_Comments__Id] on [dbo].[Comments]'
+GO
+ALTER TABLE [dbo].[Comments] ADD CONSTRAINT [PK_Comments__Id] PRIMARY KEY CLUSTERED ([Id])
+GO
+PRINT N'Creating [dbo].[Posts]'
+GO
+CREATE TABLE [dbo].[Posts]
+(
+[Id] [int] NOT NULL IDENTITY(1, 1),
+[AcceptedAnswerId] [int] NULL,
+[AnswerCount] [int] NULL,
+[Body] [nvarchar] (max) NOT NULL,
+[ClosedDate] [datetime] NULL,
+[CommentCount] [int] NULL,
+[CommunityOwnedDate] [datetime] NULL,
+[CreationDate] [datetime] NOT NULL,
+[FavoriteCount] [int] NULL,
+[LastActivityDate] [datetime] NOT NULL,
+[LastEditDate] [datetime] NULL,
+[LastEditorDisplayName] [nvarchar] (40) NULL,
+[LastEditorUserId] [int] NULL,
+[OwnerUserId] [int] NULL,
+[ParentId] [int] NULL,
+[PostTypeId] [int] NOT NULL,
+[Score] [int] NOT NULL,
+[Tags] [nvarchar] (150) NULL,
+[Title] [nvarchar] (250) NULL,
+[ViewCount] [int] NOT NULL,
+[twitter_id] [nvarchar] (20) NULL
+)
+GO
+PRINT N'Creating primary key [PK_Posts__Id] on [dbo].[Posts]'
+GO
+ALTER TABLE [dbo].[Posts] ADD CONSTRAINT [PK_Posts__Id] PRIMARY KEY CLUSTERED ([Id])
+GO
+PRINT N'Creating [dbo].[Users]'
+GO
+CREATE TABLE [dbo].[Users]
+(
+[Id] [int] NOT NULL IDENTITY(1, 1),
+[AboutMe] [nvarchar] (max) NULL,
+[Age] [int] NULL,
+[CreationDate] [datetime] NOT NULL,
+[DisplayName] [nvarchar] (40) NOT NULL,
+[DownVotes] [int] NOT NULL,
+[EmailHash] [nvarchar] (40) NULL,
+[LastAccessDate] [datetime] NOT NULL,
+[Location] [nvarchar] (100) NULL,
+[Reputation] [int] NOT NULL,
+[UpVotes] [int] NOT NULL,
+[Views] [int] NOT NULL,
+[WebsiteUrl] [nvarchar] (200) NULL,
+[AccountId] [int] NULL,
+[TikTokId] [nvarchar] (50) NULL
+)
+GO
+PRINT N'Creating primary key [PK_Users_Id] on [dbo].[Users]'
+GO
+ALTER TABLE [dbo].[Users] ADD CONSTRAINT [PK_Users_Id] PRIMARY KEY CLUSTERED ([Id])
+GO
