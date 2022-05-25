f09f371f18ff36b71ebbe08f66b501217cfda162
Andrew Pierce <andrew.craig.pierce@gmail.com>
2022-05-25 11:14:21 -0400

    looks good

---
+++ StackOverflow/migrations/V002_20220421205019__BadgesTable.sql
@@ -0,0 +1,19 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[Badges]'
+GO
+CREATE TABLE [dbo].[Badges]
+(
+[Id] [int] NOT NULL IDENTITY(1, 1),
+[Name] [nvarchar] (40) NOT NULL,
+[UserId] [int] NOT NULL,
+[Date] [datetime] NOT NULL,
+[We_dont_need_no_stinking_badges] [nvarchar] (max) NULL
+)
+GO
+PRINT N'Creating primary key [PK_Badges__Id] on [dbo].[Badges]'
+GO
+ALTER TABLE [dbo].[Badges] ADD CONSTRAINT [PK_Badges__Id] PRIMARY KEY CLUSTERED ([Id])
+GO
